73df246e16830e89468b01b8a2c43f136954f75f
Md. Rakibul Islam <39064963+Rakibul-CoU@users.noreply.github.com>
2020-09-14 12:14:26 +0600

    Add files via upload

---
+++ php/add_course.php
@@ -0,0 +1,27 @@
+<?php session_start(); ?>
+<?php include 'add_course.php'; ?>
+<?php
+  $teacher_id = $_SESSION['teacher_id'];
+  $code = strtoupper($_POST['code']);
+  $year = $_POST['year'];
+  $semester = $_POST['semester'];
+  $section = $_POST['section'];
+  $startRoll = strtoupper($_POST['start']);
+  $endRoll = strtoupper($_POST['end']);
+  $classes = $_SESSION['classes'] == 0?array():$_SESSION['classes'];
+  
+  if(verify(CODE,$code) === false) respond("error","code");
+  if(verify(NUMBER,$semester) === false) respond("error","semester");
+  if(verify(NUMBER,$year) === false) respond("error","year");
+  
+  $n = new Node($code,$teacher_id,$year,$semester) or respond("error","exists");
+  updateSession($_SESSION['email']);
+  $classes2 = $_SESSION['classes'];
+  $class_id;
+  foreach($classes2 as $c) {
+    if(!in_array($c,$classes)) $class_id = $c;
+  }
+  if(!isset($class_id)) respond("error","exists");
+  echo json_encode(array("code"=>$code,"section"=>$section,"year"=>$year,"class_id"=>$class_id));
+
+?>
---
+++ php/process_signup.php
@@ -0,0 +1,56 @@
+<?php include 'defines.php'; ?>
+<?php
+/*********** VALIDATION ************/
+foreach($_POST as $p) 
+  if(empty($p) || !isset($p))
+    respond("error","empty");
+    
+if($_POST['password'] != $_POST['password2']) 
+  respond("error","mismatch");
+if(strlen($_POST['password']) < 6) 
+  respond("error","small");
+  
+$pass = hashPass($_POST['password']);
+$email = strtolower(sqlReady($_POST['email']));
+$name = sqlReady($_POST['name']);
+$phone = sqlReady($_POST['phone']);
+/************* REGEX ************/
+if(verify(EMAIL,$email) === false) respond("error","email");
+
+$t=array("zakirjucse@gmail.com","kamal.cou@gmail.com","mahfuzcseiu01@yahoo.com","aditisarker407@gmail.com","meskat.jahan@gmail.com","cse.nayan@gmail.com","nishucse03@gmail.com","mohib.cse.bd@gmail.com","anik.com@gmail.com","knahareva@gmail.com","kamrul.hassan39@yahoo.com","hasancse03@gmail.com","parthocseju@yahoo.com","faisal_245cse@yahoo.com","mhasanraju@gmail.com","basharcse@gmail.com");
+$tt=count($t);
+$ff=false;
+for($i=0; $i<$tt; $i++)
+{
+   if($t[$i]==$email)
+{
+   $ff=true;
+break;
+}
+}
+if($ff!=true)
+respond("error","email");
+
+
+
+
+if(verify(PHONE,$phone) === false) respond("error","phone");
+if(verify(NAME,$name) === false) respond("error","name");
+
+/************ SEARCH ***********/
+$con = connectTo();
+$exists = $con->query("select * from `attendance`.`teacher` where email = '$email'");
+if($exists && $con->affected_rows) {
+  $con->close();
+  respond("error","exists");
+} 
+/*********** INSERT ************/
+$addTeacher = $con->query("insert into `attendance`.`teacher` (`name`, `email`, `phone`, `password`) values ('$name', '$email', '$phone', '$pass')");
+if(!$addTeacher && $con->errno()) {
+  $con->close();
+  die(json_encode(array("error"=>"db_error","code"=>$con->errno(),"msg"=>$con->error())));
+}
+/*********** RESPOND ************/
+$con->close();
+respond("error","none");
+?>
